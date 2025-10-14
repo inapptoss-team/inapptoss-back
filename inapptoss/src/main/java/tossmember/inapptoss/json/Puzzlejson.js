dc7057710de28bb66584bebcb978ef92b4000c0b
kmj0424 <kimminji030424@gmail.com>
2025-10-14 19:58:23 +0900

    json

---
+++ inapptoss/src/main/java/tossmember/inapptoss/json/Puzzlejson.js
@@ -0,0 +1,17 @@
+export const puzzles = {
+    'chair-puzzle' : {
+        puzzle_id : '1',
+        answer : '10001000',
+        state : 'NonDone'
+    },
+    'cabinet-puzzle' : {
+        puzzle_id : '2',
+        answer : 'Ga',
+        state : 'NonDone'
+    },
+    'mirror-puzzle': {
+        puzzle_id : '3',
+        answer : '3214',
+        state : 'NonDone'
+    },
+}
